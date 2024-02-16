b00b9769b944a999858b71388f3f88748b94d67f
Shireee <kirog11az@gmail.com>
2024-02-16 23:19:51 +0300

    --fix url decoding issues

--- lab1/server.js
+++ lab1/server.js
@@ -1,54 +1,45 @@
 http = require('http')
 const fs = require('node:fs');
 const path = require('path')
-var querystring = require("querystring");
+
 
 host = 'localhost'
 port = 3000
+
+// Define static folder 
 const public = path.join(__dirname, 'public')
 
-const listener = function(req, res) {
-    url = querystring.stringify(req.url)
-    fs.access(public + url, fs.constants.F_OK, (err) => {
-    if (err) {
-        res.writeHead(404,{'Content-type': 'text/html'});
-        res.end('404\n');
-
-        log = {
-            date: new Date(),
-            ip: req.socket.remoteAddress,
-            path: req.url,
-            code: res.statusCode
-        }
-    
-        saveLog(log)
-    } else {
-        const readStream = fs.createReadStream(public + url);
-        res.writeHead(200,{'Content-type': 'text/html'});
-        readStream.pipe(res);
-    
-        log = {
-            date: new Date(),
-            ip: req.socket.remoteAddress,
-            path: req.url,
-            code: res.statusCode
-        }
-    
-        saveLog(log)
-        }
-    })
-}
-
-const server = http.createServer(listener);
-
-server.listen(port, host, () => {
-    console.log(`Servet listening on port: ${port}`)
-})
-
 // Logging function 
-function saveLog(log){
+function saveLog(ip, date, url, code){
     fs.writeFile('./log.txt', 
-                JSON.stringify(log) + '\r\n',
+                JSON.stringify({ date: date, ip: ip, path: url, code: code}) + '\r\n',
                 {flag: 'a+'}, 
                 err => { err ? console.log(err) : console.log(`Logged: ${new Date()}`)})
 }
+
+// Create a listener
+const listener = function(req, res) {
+    const url = decodeURIComponent(req.url); 
+    const filePath = path.join(public, url);
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) { // handle non-existing path
+            res.writeHead(404, {'Content-type': 'text/html'});
+            res.end('404\n');
+            saveLog(req.socket.remoteAddress, new Date(), url, res.statusCode);
+        } else {
+            const readStream = fs.createReadStream(filePath);
+            res.writeHead(200);
+            readStream.pipe(res);
+            saveLog(req.socket.remoteAddress, new Date(), url, res.statusCode);
+        }
+    });
+};
+
+// Create a server instance 
+const server = http.createServer(listener);
+
+// Run server
+server.listen(port, host, () => {
+    console.log(`Servet listening on port: ${port}`)
+})
