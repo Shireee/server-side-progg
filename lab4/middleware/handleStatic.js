6a93ce94ec5379482c5238c47cc2f4fbaacc542a
Shireee <kirog11az@gmail.com>
2024-04-01 23:36:56 +0300

    Decomposition of structure

---
+++ lab4/middleware/handleStatic.js
@@ -0,0 +1,20 @@
+const fs = require('node:fs');
+const path = require('path');
+
+// Function for handling response with static files
+
+function handleStatic(req, res) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    fs.readFile(path.join(path.dirname(__dirname), 'public', decodeURIComponent(url.pathname)), (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            res.end("404");
+            return;
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+module.exports = { handleStatic };
